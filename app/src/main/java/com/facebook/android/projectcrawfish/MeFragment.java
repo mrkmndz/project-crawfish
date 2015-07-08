4b436333d8ce2088524e94ff95ea8e50776bd76a
Grace Kao <kaogra@fb.com>
2015-07-08 16:21:36 -0700

    Me tab profile card
    Summary: Added profile card to me tab along with colored facebook and linkedin icons
    
    Test Plan: Build and run. Check format, sizing, color.
    
    Reviewers: connie, mariazlatkova, markamendoza
    
    Reviewed By: mariazlatkova, markamendoza
    
    Differential Revision: https://phabricator.fb.com/D2228959
    
    Signature: t1:2228959:1436401734:7271fe05a02bdb1414660f164dfedca939bcf431

--- app/src/main/java/com/facebook/android/projectcrawfish/MeFragment.java
+++ app/src/main/java/com/facebook/android/projectcrawfish/MeFragment.java
@@ -8,6 +8,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.joanzapata.android.iconify.Iconify;
+import com.joanzapata.android.iconify.IconDrawable;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 /**
@@ -16,16 +23,31 @@ import android.view.ViewGroup;
 public class MeFragment extends Fragment {
 
 
+
     public MeFragment() {
         // Required empty public constructor
     }
+
+    @Bind(R.id.contact_fb)
+    ImageButton mFacebook;
+
+    @Bind((R.id.contact_linkedin))
+    ImageButton mLinkedIn;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_me, container, false);
+
+        View v= inflater.inflate(R.layout.fragment_me, container, false);
+        ButterKnife.bind(this, v);
+        mFacebook.setImageDrawable(new IconDrawable(getActivity(), Iconify.IconValue.fa_facebook_square).sizeDp(48).colorRes(R.color.fb));
+        mLinkedIn.setImageDrawable(new IconDrawable(getActivity(), Iconify.IconValue.fa_linkedin_square).sizeDp(48).colorRes(R.color.linkedIn));
+
+
+
+
+        return v;
     }
 
 
